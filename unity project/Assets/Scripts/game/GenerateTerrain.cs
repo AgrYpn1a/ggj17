2cbda680f6c153f37b8f731bd25e0269334a5998
Rastko <rastkotojagic1996@gmail.com>
2017-01-21 20:08:50 +0100

    almost done with terrain

--- unity project/Assets/Scripts/game/GenerateTerrain.cs
+++ unity project/Assets/Scripts/game/GenerateTerrain.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -25,6 +26,7 @@ public class GenerateTerrain : MonoBehaviour
     [SerializeField]
     private int numberOfFrets = 10;
     private Transform[] frets;
+    private Transform[] fretsTop;
 
     [SerializeField]
     private Transform player;
@@ -33,10 +35,14 @@ public class GenerateTerrain : MonoBehaviour
 
     [SerializeField]
     private int defHeight = 2;
-    
+    [SerializeField]
+    private int defHeightTop = 2;
+
     void Start()
     {
         frets = new Transform[numberOfFrets];
+        fretsTop = new Transform[numberOfFrets];
+
         changePoint = frets.Length / 2 - 1;
 
         generateFrets();
@@ -49,7 +55,7 @@ public class GenerateTerrain : MonoBehaviour
         {
             if (first)
             {
-                RecycleFrets(0, changePoint, frets.Length-1);
+                RecycleFrets(0, changePoint, frets.Length - 1);
                 first = false;
             }
             else
@@ -78,9 +84,15 @@ public class GenerateTerrain : MonoBehaviour
             start.position,
             Quaternion.identity) as Transform;
 
+        Transform firstTop = Instantiate(whiteFret,
+            start.position,
+            Quaternion.identity) as Transform;
+
         Vector3 lastPos = first.position;
 
         frets[0] = first;
+        fretsTop[0] = firstTop;
+
         degrees = addOne(degrees, 360);
 
         for (int counter = 1; counter < frets.Length; counter++)
@@ -89,37 +101,52 @@ public class GenerateTerrain : MonoBehaviour
                 lastPos + new Vector3(transform.position.x + fretWidth, 0, 0),
                 Quaternion.identity) as Transform;
 
+            Transform currentTop = Instantiate(whiteFret,
+                lastPos + new Vector3(transform.position.x + fretWidth, 0, 0),
+                Quaternion.identity) as Transform;
+
             lastPos = current.position;
             frets[counter] = current;
+            fretsTop[counter] = currentTop;
 
-            SetHeight(current);
+            SetHeight(current, defHeight);
+
+
+            // update top
+            fretsTop[counter].position = new Vector3(fretsTop[counter].position.x, frets[counter].position.y + defHeightTop, 0);
         }
     }
 
     private void RecycleFrets(int begin, int end, int pos)
     {
-        Debug.Log("called recycle");
         currentStep = 0;
         //start.position = frets[frets.Length - 1].position + new Vector3(fretWidth, 0, 0);
 
         // get position of the last
         Vector3 currentPos = frets[pos].position;
+
         for (int counter = begin; counter < end; counter++)
         {
             // always add to last
             frets[counter].position = currentPos + new Vector3(fretWidth, 0, 0);
+            fretsTop[counter].position = currentPos + new Vector3(fretWidth, 0, 0);
+
             currentPos = frets[counter].position;
 
-            SetHeight(frets[counter]);
+            Transform current = frets[counter];
 
+            SetHeight(current, defHeight);
+            
+            // update top
+            fretsTop[counter].position = new Vector3(fretsTop[counter].position.x, current.position.y + defHeightTop, 0);
         }
 
     }
 
-    private void SetHeight(Transform t)
+    private void SetHeight(Transform t, float defHeight)
     {
         degrees = addOne(degrees, 360);
-        float y = defHeight + Mathf.Sin(degrees) * UnityEngine.Random.Range(0.1f, 1.6f);
+        float y = defHeight + Mathf.Sin(degrees) * UnityEngine.Random.Range(0.2f, 2.0f);
         t.transform.position = new Vector3(t.transform.position.x, y, 0);
     }
 }
