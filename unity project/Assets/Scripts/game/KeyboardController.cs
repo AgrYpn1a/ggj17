6d167010a1a169032a12327c95423b22c8fc9ffc
nemanjagajic <Nemanja.gajicru96@gmail.com>
2017-01-22 08:06:05 +0100

    Rastko slinic

---
+++ unity project/Assets/Scripts/game/KeyboardController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardController : MonoBehaviour {
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private float speed;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow))
+        {
+            player.transform.position += new Vector3(0, speed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            player.transform.position += new Vector3(0, -speed, 0);
+        }
+
+        player.transform.position += new Vector3(speed, 0, 0);
+    }
+
+}
